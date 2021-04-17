2d132f0938e212c27f09a12b51d0e79508cafda6
Walter Morvillo <w.morvillo@live.com>
2021-04-16 23:26:07 -0600

    Version 1

---
+++ plots.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+
+
+country<-"Canada"
+
+canada_vaxx_data<-cumulative_covid_vaccinations%>%set_names(c("Country","Code","Date","total_vaxx"))%>%filter(Country==country)
+
+canada_case_data<-owid_covid_data%>%filter(location==country)%>%select(date,new_cases)%>%
+  set_names(c("Date","new_cases"))
+
+table<-left_join(canada_case_data,canada_vaxx_data,by="Date")%>%
+  select(Date,new_cases,total_vaxx)%>%mutate(vaxx_per_thousand=total_vaxx/1000)
+
+
+data <- c("New Cases","Total Vaccinated")
+labels <- data.frame(data = data, x = c(2020-09-01,2021-04-12), y = c(2500,2000))
+
+
+table%>%
+  ggplot()+
+  geom_point(aes(Date,new_cases),col="red")+
+  geom_point(aes(Date,vaxx_per_thousand))+
+  xlab("Date") +
+  ylab("New Daily Cases") +
+  ggtitle("Comparing new cases to vaccine cases/1000")
+  
+
+
+  
+
+
+ 
