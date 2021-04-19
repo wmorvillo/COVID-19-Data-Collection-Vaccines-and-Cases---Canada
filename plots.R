bf8e7ad071b1a101df763a3eb4eeaa7ebcc2c14d
Walter Morvillo <w.morvillo@live.com>
2021-04-18 19:31:11 -0600

    Version 3

--- plots.R
+++ plots.R
@@ -4,24 +4,29 @@ library(ggplot2)
 library(stringr)
 
 
-country<-"Canada"
 
-canada_vaxx_data<-cumulative_covid_vaccinations%>%set_names(c("Country","Code","Date","total_vaxx"))%>%filter(Country==country)
+country<-"Brazil"
 
-canada_case_data<-owid_covid_data%>%filter(location==country)%>%select(date,new_cases)%>%
+vaxx_data<-cumulative_covid_vaccinations%>%set_names(c("Country","Code","Date","total_vaxx"))%>%filter(Country==country)
+
+case_data<-owid_covid_data%>%filter(location==country)%>%select(date,new_cases)%>%
   set_names(c("Date","new_cases"))
 
-table<-left_join(canada_case_data,canada_vaxx_data,by="Date")%>%
-  select(Date,new_cases,total_vaxx)%>%mutate(vaxx_per_thousand=total_vaxx/1000)
+table<-left_join(case_data,vaxx_data,by="Date")%>%
+  select(Date,new_cases,total_vaxx)%>%mutate(vaxx_per_thousand=total_vaxx/100)
+
+first_vax<-table$Date[which.min(table$total_vaxx)]
 
 
 table%>%
   ggplot()+
   geom_point(aes(Date,new_cases),col="red")+
-  geom_point(aes(Date,vaxx_per_thousand))+
+  geom_vline(xintercept=first_vax)+
+  ylim(-100, (max(table$new_cases))*1.15)+
   xlab("Date") +
   ylab("New Daily Cases") +
-  ggtitle("Comparing new cases to vaccine cases/1000")
+  ggtitle("Comparing new cases to first Vaccines administered")
+ 
   
 
 
