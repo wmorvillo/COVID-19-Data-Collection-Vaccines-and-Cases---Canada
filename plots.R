d78a2f93c6827fb88b92b2ade2676f878b5913b8
Walter Morvillo <w.morvillo@live.com>
2021-04-19 21:43:18 -0600

    Version 4

--- plots.R
+++ plots.R
@@ -2,27 +2,30 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(stringr)
+library(readr)
+
+cumulative_covid_vaccinations <- read_csv("cumulative-covid-vaccinations.csv")
+owid_covid_data <- read_csv("owid-covid-data.csv")
 
 
 
-country<-"Brazil"
+country<-"Canada"
 
-vaxx_data<-cumulative_covid_vaccinations%>%set_names(c("Country","Code","Date","total_vaxx"))%>%filter(Country==country)
+vaxx_data<-cumulative_covid_vaccinations%>%
+  set_names(c("Country","Code","Date","total_vaxx"))%>%
+  filter(Country==country)
 
 case_data<-owid_covid_data%>%filter(location==country)%>%select(date,new_cases)%>%
   set_names(c("Date","new_cases"))
 
-table<-left_join(case_data,vaxx_data,by="Date")%>%
+custom_table<-left_join(case_data,vaxx_data,by="Date")%>%
   select(Date,new_cases,total_vaxx)%>%mutate(vaxx_per_thousand=total_vaxx/100)
 
-first_vax<-table$Date[which.min(table$total_vaxx)]
-
-
-table%>%
+custom_table%>%
   ggplot()+
   geom_point(aes(Date,new_cases),col="red")+
-  geom_vline(xintercept=first_vax)+
-  ylim(-100, (max(table$new_cases))*1.15)+
+  geom_vline(xintercept=custom_table$Date[which.min(custom_table$total_vaxx)])+
+  ylim(-100, (max(custom_table$new_cases))*1.15)+
   xlab("Date") +
   ylab("New Daily Cases") +
   ggtitle("Comparing new cases to first Vaccines administered")
