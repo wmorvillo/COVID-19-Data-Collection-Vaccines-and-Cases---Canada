9605218dcc8291dc0e8d164e5f27534f46402b64
Walter Morvillo <w.morvillo@live.com>
2021-04-24 21:02:03 -0600

    Version 7

--- plots.R
+++ plots.R
@@ -7,15 +7,19 @@ library(rvest)
 
 cumulative_covid_vaccinations <- read_csv("cumulative-covid-vaccinations.csv")
 owid_covid_data <- read_csv("owid-covid-data.csv")
+url<-"https://www.worldometers.info/world-population/population-by-country/"
 
-url<-"https://data.cdc.gov/Case-Surveillance/United-States-COVID-19-Cases-and-Deaths-by-State-o/9mfq-cb36"
-states<-read_html(url)
+country<-"Canada"
 
-#test<-states%>%html_nodes(".socrata-visualization-chart-container")%>%html_text()
+pop<-read_html(url)%>%
+  html_nodes("table")%>%
+  html_table()%>%
+  .[[1]]%>%setNames(c("#","Country","Population"))%>%
+  select(Country, Population)%>%
+  filter(Country==country)%>%
+  mutate_at(2,parse_number)%>%
+  .$Population
   
-
-country<-"United States"
-
 death_data<-owid_covid_data%>%filter(location==country)%>%
   select(date,location,new_deaths)%>%
   set_names(c("Date","Country","New_Deaths"))
@@ -32,12 +36,16 @@ custom_table<-left_join(case_data,vaxx_data, by = "Date")%>%
 
 custom_table<-left_join(death_data,custom_table,by = "Date")
 
+number_vaccinated<-max(vaxx_data$total_vaxx)
+
+num_vax_percentage<-(number_vaccinated/pop)*100
+
 custom_table%>%
   ggplot()+
   geom_point(aes(Date,new_cases),col="red")+
-  geom_point(aes(Date,New_Deaths))+
+  #geom_point(aes(Date,New_Deaths))+
   geom_vline(xintercept=custom_table$Date[which.min(custom_table$total_vaxx)])+
-  ylim(-100, (max(custom_table$new_cases))*1.15)+
+  #ylim(-100, (max(custom_table$new_cases))*1.15)+
   xlab("Date") +
   ylab("New Daily Cases") +
   ggtitle("Comparing new cases to first Vaccines administered")
