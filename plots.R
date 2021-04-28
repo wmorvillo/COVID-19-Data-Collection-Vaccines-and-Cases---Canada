a73861be7758ea4c2cb5fae3e4ea9a8f793a5a92
Walter Morvillo <w.morvillo@live.com>
2021-04-27 20:41:14 -0600

    Version 10

--- plots.R
+++ plots.R
@@ -9,8 +9,9 @@ cumulative_covid_vaccinations <- read_csv("cumulative-covid-vaccinations.csv")
 owid_covid_data <- read_csv("owid-covid-data.csv")
 url<-"https://www.worldometers.info/world-population/population-by-country/"
 
-#set.seed(20)
 
+
+#######
 number_of_countries<-3
 country_index<-0
 for(i in 1:number_of_countries)
@@ -35,7 +36,8 @@ pop<-read_html(url)%>%
 
 
 #######  
-death_data<-owid_covid_data%>%filter(location%in%country_df$Country & !is.na(continent))%>%
+death_data<-owid_covid_data%>%
+  filter(location%in%country_df$Country & !is.na(continent))%>%
   select(date,location,new_deaths)%>%
   set_names(c("Date","Country","New_Deaths"))%>%na.omit(New_Deaths)
   
@@ -45,16 +47,15 @@ vaxx_data<-cumulative_covid_vaccinations%>%
   filter(Country%in%country_df$Country)%>%
   select(Date,Country,total_vaxx)
 
+#######
 test<-setdiff(country_df$Country,vaxx_data$Country)
 temp_df<-data.frame(Date=NA,Country=NA, total_vaxx=NA)
 if(is.null(test)){
   temp_df<-data.frame(Date=NA, Country = test,total_vaxx=NA)
 }
 
-
+#######
 first_vaxx_date<-0
-
-
 for(j in 1:number_of_countries)
 {
   first_vaxx<-vaxx_data%>%filter(total_vaxx != 0)%>%rbind(temp_df)%>%
@@ -67,24 +68,13 @@ first_vaxx_date<-gather(first_vaxx_date,key=data,value = Date)%>%select(Date)
 first_vaxx_date<-bind_cols(first_vaxx_date,country_df)
 
 
-case_data<-owid_covid_data%>%filter(location%in%country_df$Country)%>%select(date, location, new_cases)%>%
+case_data<-owid_covid_data%>%filter(location%in%country_df$Country)%>%
+  select(date, location, new_cases)%>%
   set_names(c("Date","Country","new_cases"))%>%na.omit()
 
 Full_Data<-left_join(case_data,death_data,by=c("Date","Country"))
   
-
-#custom_table<-left_join(case_data,vaxx_data, by = "Date")#%>%
-#  select(Date,Country,new_cases,total_vaxx)%>%left_join(death_data,by="Date")%>%
-#  select(Date,new_cases,New_Deaths,total_vaxx)
-
-#test<-data.frame(test=which(is.na(owid_covid_data$continent)))
-
-#######
-#number_vaccinated<-max(vaxx_data$total_vaxx)
-#num_vax_percentage<-(number_vaccinated/pop)*100
-
-
-#######
+######
 Full_Data%>%group_by(Country)%>%
   ggplot()+
   geom_point(aes(Date,New_Deaths,col=Country))+
